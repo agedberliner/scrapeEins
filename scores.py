7703808e73a452a6df4f2603d8f8ac0e1bad3040
agedberliner <da.berger98@gmail.com>
2022-08-31 09:09:01 +0200

    31.8.22 added comments 28.8.22 rouge scores and operative commit 27.7.22 finalizing and skipping certain articles 20.7.22 grouping of paragraphs by color 16.6.22 brand eins first complete scrape

--- scores.py
+++ scores.py
@@ -7,11 +7,12 @@ rouge = Rouge()
 with open('brandeins.csv', newline='', encoding="utf-8") as f:
     reader = csv.reader(f)
     header = next(reader)
-    comple = []
-    compleinv = []
-    simple = []
-    simpleinv = []
+    comple = []  # list for all complex paragraphs
+    compleinv = []  # list for all complex paragraphs in reverse order
+    simple = []  # list for all simple paragraphs
+    simpleinv = []  # list for all simple paragraphs in inverse order
 
+    # filling the lists with content from the brandeins.csv file
     for row in reader:
         if len(row) == 0:
             continue
@@ -20,6 +21,8 @@ with open('brandeins.csv', newline='', encoding="utf-8") as f:
         simple.append(row[1])
         simpleinv.insert(0, row[1])
 
+
+# calculating scores of different combinations of complex and simple and "inverse" as random data
 scoresComSim = rouge.get_scores(comple, simple)
 scoresComSimInv = rouge.get_scores(comple, simpleinv)
 scoresComComInv = rouge.get_scores(comple, compleinv)
@@ -28,34 +31,43 @@ scoresSimCom = rouge.get_scores(simple, comple)
 scoresSimComInv = rouge.get_scores(simple, compleinv)
 scoresSimSimInv = rouge.get_scores(simple, simpleinv)
 
-
-rouge1 = []
-rouge2 = []
-rougel = []
-
-
-def calculaterouge(scores):
+# get the relevant data from the calculated rouge score
+def getrouge1(scores):
     counter = 0
+    rouge1 = []
     while counter < len(scores):
         rouge1.append(scores[counter]["rouge-1"]["f"])
+        counter = counter + 1
+    return rouge1
+
+def getrouge2(scores):
+    counter = 0
+    rouge2 = []
+    while counter < len(scores):
         rouge2.append(scores[counter]["rouge-2"]["f"])
+        counter = counter + 1
+    return rouge2
+
+def getrougel(scores):
+    counter = 0
+    rougel = []
+    while counter < len(scores):
         rougel.append(scores[counter]["rouge-l"]["f"])
         counter = counter + 1
+    return rougel
 
 
-calculaterouge(scoresSimCom)
-
-
+# normalize y axis from 0-1
 yaxis = plt.gca()
 yaxis.set_ylim([0, 1])
 
-plt.plot(rouge1)
+
+plt.plot(getrougel(scoresComSim))
+plt.plot(getrougel(scoresComSimInv))
 plt.show()
 
 
-log = False
-
 
 def log(string):
-    if log:
+    if False:
         print(string)
